44df0a03e8305e7b94d6e97cdc7d4141ef29cf46
crocodile Stingray <whdtjs152413@gmail.com>
2023-12-06 12:43:35 +0900

    1. 실행 환경 및 빌드 구성

---
+++ src/main/java/com/example/study/entity/Team.java
@@ -0,0 +1,27 @@
+package com.example.study.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@ToString(exclude = "members") // 무한루프에 빠지지 않게 양방향 조건
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_name")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList();
+}
