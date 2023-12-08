b135dc9892bd03bc9d2851260cc2166a3a297f8a
crocodile Stingray <whdtjs152413@gmail.com>
2023-12-08 10:51:01 +0900

    4. QueryDSL의 다양한 문법들, QueryDSL 객체화, (동적 sql 활용법)

--- src/main/java/com/example/study/repository/MemberRepository.java
+++ src/main/java/com/example/study/repository/MemberRepository.java
@@ -3,7 +3,11 @@ package com.example.study.repository;
 import com.example.study.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MemberRepository extends JpaRepository<Member, Long>,
         MemberRepositoryCustom{
+
+
+
 
 }
